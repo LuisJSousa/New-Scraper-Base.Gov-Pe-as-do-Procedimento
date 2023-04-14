4fe2d66f0ffc399ebdbd1b23ba771407eda4fd97
Luís J. Sousa <94967351+LuisJSousa@users.noreply.github.com>
2023-04-14 16:08:54 +0100

    Update base.py

--- base.py
+++ base.py
@@ -105,8 +105,89 @@ while True:
                 utils.download_vortal(link_value=link_value, driver=driver, link_element=link_element, wait=wait)
                 driver.switch_to.window(driver.window_handles[-1])
                 driver.close()
-                print("AAAAAAAAAAAAAAAAAAAA")
                 print(len(driver.window_handles))
+            elif "anogov" in link_value:
+                link_anogov = link_value.split("/")[-1]
+                link_anogov = link_value.replace(link_anogov,"")
+                print("-----É ANOGOV-----")
+                print(link_anogov)
+                link_value = os.path.basename(link_value).replace('/', '_').replace(':', '_').replace(" ","_").replace(".", "_").replace("=", "_").replace("?","_")
+                if not os.path.exists(f'Filedump/{link_value}'):
+                    os.makedirs(f'Filedump/{link_value}')
+                link_element.click()
+                wait.until(EC.number_of_windows_to_be(3))
+                wait.until(EC.new_window_is_opened)
+                time.sleep(3)
+                driver.switch_to.window(driver.window_handles[-1])
+                anogov_table = wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="listaDocs:tabelaDocumentos"]')))
+                tbody_anogov = anogov_table.find_element(By.TAG_NAME, "tbody")
+                tbody_html = tbody_anogov.get_attribute("outerHTML") #for print porpuses
+                num_rows = len(tbody_anogov.find_elements(By.TAG_NAME, "tr"))
+                lista_html = []
+                while True:
+                    for i in range(num_rows + 3*num_rows-1):
+                        try:
+                            anogov_table = wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="listaDocs:tabelaDocumentos"]')))
+                            tbody_anogov = anogov_table.find_element(By.TAG_NAME, "tbody")
+                            rows_anogov = tbody_anogov.find_elements(By.TAG_NAME, "tr")
+                            row_index = i 
+                            num_rows = len(tbody_anogov.find_elements(By.TAG_NAME, "tr"))
+                            if (i !=0):
+                                row_index += 1
+                                print(row_index)   
+                            row_anogov = rows_anogov[row_index]
+                            texto = row_anogov.get_attribute("outerHTML")
+                            lista_html.append(texto)
+                            #print("row: ", row_anogov)
+                            #print("row html: ", texto)
+                            wait2 = WebDriverWait(row_anogov, 3) 
+                            time.sleep(2)
+                            if row_anogov in rows_anogov:
+                                try:
+                                    hover_icon = wait2.until(EC.presence_of_element_located((By.CLASS_NAME, 'options-column')))
+                                    # hover over the icon to make the button appear
+                                    ActionChains(driver).move_to_element(hover_icon).perform()
+                                    # wait for the button to be clickable
+                                    button = wait2.until(EC.element_to_be_clickable((By.CLASS_NAME, 'options')))          
+                                    button.click()
+                                except:
+                                    continue
+                        except  IndexError:
+                            continue        
+                    for item in lista_html:
+                        soup = BeautifulSoup(item, 'html.parser')
+                        lista_de_requests = []
+                        try:
+                            link = soup.find('a', href=lambda href: href and 'downloadDiretoDocumento' in href)
+                            sequence = link['href']
+                            request_anogov = link_anogov + sequence
+                            response_anogov = requests.get(request_anogov)
+                            content_disposition_ano = response_anogov.headers.get('Content-Disposition')
+                            print("ano", content_disposition_ano)
+                            filename_anogov = content_disposition_ano.split("''")[1]
+                            filename_anogov = filename_anogov.split(".")[0]
+                            print("file", filename_anogov) 
+                                                        
+                            if ".pdf" in content_disposition_ano:
+                                with open(f"Filedump/{link_value}/{filename_anogov}.pdf", "wb") as f:
+                                    f.write(response_anogov.content)
+                            elif ".xlsx" in content_disposition_ano:
+                                with open(f"Filedump/{link_value}/{filename_anogov}.xlsx", "wb") as f:
+                                    f.write(response_anogov.content)
+                            else:
+                                with open(f"Filedump/{link_value}/{filename_anogov}.xls", "wb") as f:
+                                    f.write(response_anogov.content)
+                        except:
+                            continue
+                    
+                    try:
+                        next_page_anogov = driver.find_element(By.XPATH, '//*[@id="listaDocs:pag"]/tbody/tr/td[3]/div/input[3]')
+                        next_page_anogov.click()
+                        time.sleep(3)
+                    except:
+                        break
+                driver.switch_to.window(driver.window_handles[-1])
+                driver.close()   
             else:
                 try:
                     # download file to 'Filedump' folder
@@ -126,11 +207,11 @@ while True:
         
             
         # close the current window and switch back to the original window
-        print("AAAAAAAAAAAAAAAAAAAA")
+        
         print(len(driver.window_handles))
         driver.switch_to.window(driver.window_handles[-1])
         driver.close()
-        print("AAAAAAAAAAAAAAAAAAAA")
+        
         print(len(driver.window_handles))
         driver.switch_to.window(driver.window_handles[0])
 
